85b08578263186c9af5a374114ad93357f939354
Vladimir <45636169+softmasterlab@users.noreply.github.com>
2021-09-22 21:33:46 +0300

    Финальная версия

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from config import mysql
+from werkzeug.security import check_password_hash
 
 
 class User(object):
@@ -6,7 +7,7 @@ class User(object):
     @staticmethod
     def register(login, password, email, regdate, status) -> None:
         connection = mysql.get_db()
-        cursor = connection.cursor()  # создаем курсор
+        cursor = connection.cursor()
         sql_query = """
             insert into users (login, password, email, regdate, status)
             values (%s, %s, %s, %s, %s)
@@ -17,5 +18,20 @@ class User(object):
         connection.close()
 
     @staticmethod
-    def login() -> None:
-        pass
+    def auth(login, password) -> bool:
+        connection = mysql.get_db()
+        cursor = connection.cursor()
+        sql_query = """
+            select login, password from users where login=%s
+        """
+        cursor.execute(sql_query, (login,))
+        result = cursor.fetchall()
+        k = len(result)
+
+        cursor.close()
+        connection.close()
+
+        if k == 0:
+            return False
+        else:
+            return check_password_hash(result[0][1], password)
